6482fdcb4975ad425a991a3114bc532ebf121cc3
LukaszSobczak <sobluxx@gmail.com>
2022-10-04 03:44:50 -0500

    saving result as csv, removing marker from image

--- src/aruco_labeling.py
+++ src/aruco_labeling.py
@@ -7,8 +7,10 @@ import cv2
 from cv2 import aruco
 from threading import Lock, Thread
 import time
+from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 from enum import Enum
+from pathlib import Path
 
 import tensorflow as tf
 from tensorflow.keras.applications import mobilenet
@@ -47,9 +49,9 @@ class ClassificationManager:
             x = mobilenet.preprocess_input(x)
             return self.model.predict(x, verbose=self.verbose)
 
-    def get_printable_predictions(self, preds):
+    def get_printable_predictions(self, preds, top=3):
         if self.model_id == Model.MOBILE_NET_V1:
-            return mobilenet.decode_predictions(preds, top=3)[0]
+            return mobilenet.decode_predictions(preds, top=top)[0]
 
 class ArucoManager:
     def __init__(self):
@@ -64,19 +66,27 @@ class ArucoManager:
     def draw(self, image, corners, ids):
         return aruco.drawDetectedMarkers(image.copy(), corners, ids)
 
-class Node():
+class LabelingNode():
     def __init__(self):
+        self.model_id = Model.MOBILE_NET_V1
         self.aruco = ArucoManager()
-        self.ml = ClassificationManager(Model.MOBILE_NET_V1, 1)
+        self.ml = ClassificationManager(self.model_id, 1)
 
         self.message_lock = Lock()
         self.thread_executor = ThreadPoolExecutor(5)
-        self.ml_future = self.thread_executor.submit(self.preprocess_and_detect, None, None)
+        self.ml_future = self.thread_executor.submit(self.preprocess_and_detect, None, None, None)
 
         self.image = None
         self.br = CvBridge()
         self.sub = rospy.Subscriber("~camera_in", Image, self.image_callback)
         self.pub = rospy.Publisher('~aruco_image', Image, queue_size=10)
+        self.detection_period = rospy.Duration(rospy.get_param('~detection_period', 5))
+        self.last_detection_time = rospy.Time.from_sec(0)
+        self.result_filename = None
+        self.result_counter = 0
+        self.output_dir = Path(rospy.get_param('~output_dir', ""))
+        self.output_dir = self.output_dir / datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+        self.output_dir.mkdir(parents=True, exist_ok=True)
 
     def image_callback(self, msg):
         with self.message_lock:
@@ -92,7 +102,7 @@ class Node():
                     if ids is not None and len(ids) > 0:
                         if self.ml_future.done():
                             self.ml_future.result()
-                            self.ml_future = self.thread_executor.submit(self.preprocess_and_detect, self.image.copy(), ids.copy())
+                            self.ml_future = self.thread_executor.submit(self.preprocess_and_detect, self.image.copy(), ids.copy(), corners.copy())
                     self.image = None
 
             loop_rate.sleep()
@@ -104,19 +114,58 @@ class Node():
             msg.header.frame_id = "camera_link"
             self.pub.publish(msg)
 
-    def preprocess_and_detect(self, image, ids):
-        if image is None or ids is None:
+    def preprocess_and_detect(self, image, ids, corners):
+        if image is None or ids is None or corners is None:
             return False
 
         # example ids for 1 detected marker: [[0]]
         # example ids for 2 detected markers: [[1][0]]
         if len(ids) == 1 and len(ids[0]) == 1:
+            if rospy.Time.now() - self.last_detection_time < self.detection_period:
+                return False
+
+            image = self.remove_marker(image, np.array(corners[0][0]))
+            image = self.blur_background(image)
+
             preds = self.ml.predict(image)
             print(f"Aruco id: {ids[0][0]} - predicted: {self.ml.get_printable_predictions(preds)}")
+            self.store_result(image, ids[0][0], np.argmax(preds, axis=1)[0])
+            self.last_detection_time = rospy.Time.now()
 
         return True
 
+    def store_result(self, image, aruco_id, class_id):
+        if self.result_filename is None:
+            self.result_filename = f"{self.model_id.name}_labeling.csv"
+            self.result_filename = self.output_dir / self.result_filename
+            np.savetxt(self.result_filename, [["id, image_name, aruco_id, class_id"]], delimiter=',', fmt='%s')
+
+        image_name = f"{self.result_counter}.jpg"
+        cv2.imwrite(str(self.output_dir / image_name), image)
+        with open(self.result_filename,'a') as csvfile:
+            np.savetxt(csvfile, [[self.result_counter, image_name, aruco_id, class_id]], delimiter=',', fmt='%s')
+        self.result_counter += 1
+
+    def remove_marker(self, image, corners):
+        x = np.array([corners[0][0], corners[1][0], corners[2][0], corners[3][0]])
+        y = np.array([corners[0][1], corners[1][1], corners[2][1], corners[3][1]])
+        min_x = int(np.amin(x))
+        max_x = int(np.amax(x))
+        min_y = int(np.amin(y))
+        max_y = int(np.amax(y))
+        shift = 2
+        mean_color = ((image[max_y+shift, max_x+shift, :].astype(int) + 
+                       image[min_y-shift, max_x+shift, :].astype(int) + 
+                       image[max_y+shift, min_x-shift, :].astype(int) + 
+                       image[min_y-shift, min_x-shift, :].astype(int)) / 4).astype(np.uint8, casting='unsafe')
+        image[min_y:max_y, min_x:max_x, :] = mean_color
+        return image
+
+    def blur_background(self, image):
+        # TODO: bluring background
+        return image
+
 if __name__ == '__main__':
     rospy.init_node("aruco_labeling", anonymous=True)
-    my_node = Node()
+    my_node = LabelingNode()
     my_node.spin()
